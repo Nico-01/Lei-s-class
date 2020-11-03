1d2568cee6a8002a033bc838d419c4a37750b89c
Nico-01 <nicolegaskell.contact@gmail.com>
2020-11-04 10:24:12 +1300

    myfirstconnection.php

--- phpAndDatabase/myfirstconnection.php
+++ phpAndDatabase/myfirstconnection.php
@@ -1,13 +1,13 @@
 <?php
 // Create a database connection
-$server = "u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-$dbusername = "qrzgb3mx9k0wl0ko"
-$dbpassword = "p2hohc6r9awsbfjy"
-$dbname = "d9jdge5z2cy7qr3m"
+$server = "u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "qrzgb3mx9k0wl0ko";
+$dbpassword = "p2hohc6r9awsbfjy";
+$dbname = "d9jdge5z2cy7qr3m";
 
 $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
-$if ($conn->error){
+if ($conn->error){
      echo $conn->error;
 }else{
     echo "Connected";
