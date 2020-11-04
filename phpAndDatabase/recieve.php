5c9304dfe7aa9d1957fda99e8b572ef224457d16
Nico-01 <nicolegaskell.contact@gmail.com>
2020-11-05 09:25:55 +1300

    created a recieve page.php

---
+++ phpAndDatabase/recieve.php
@@ -0,0 +1,13 @@
+<?php
+$username =_$GET["username"];
+$password =_$GET["password"];
+$number =_$GET["myNumber"];
+$color =_$GET["myColor"];
+$select =_$GET["mySelect"];
+
+echo "Username: ".$username;
+echo "<br>Password: ".$password;
+echo "<br>Number: ".$number;
+echo "<br>Color: ".$color;
+echo "<br>Selection: ".$select;
+
