f89f8b3936bb68219f92fc97771bebba06010815
Nico-01 <nicolegaskell.contact@gmail.com>
2020-11-09 09:31:23 +1300

    added page additions, test 1

--- SessionExercise/page1.php
+++ SessionExercise/page1.php
@@ -4,3 +4,12 @@
 //get session's value
 $username = $_SESSION["username"];
 echo "Username:".$username;
+
+$password = $_SESSION["password"];
+echo "Password:".$password;
+
+$color = $_SESSION["color"];
+echo "Color:".$color;
+
+$address = $_SESSION["Address"];
+echo "Address:".$address;
--- SessionExercise/page3.php
+++ SessionExercise/page3.php
@@ -1,1 +1,15 @@
 <?php
+//start a session
+@session_start();
+//get session's value
+$username = $_SESSION["username"];
+echo "Username:".$username;
+
+$password = $_SESSION["password"];
+echo "Password:".$password;
+
+$color = $_SESSION["color"];
+echo "Color:".$color;
+
+$address = $_SESSION["Address"];
+echo "Address:".$address;
