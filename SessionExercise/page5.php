ba558ef284eb392720bbbb4d20f93588c66e924e
Nico-01 <nicolegaskell.contact@gmail.com>
2020-11-09 09:42:44 +1300

    created session destroy on page 5

--- SessionExercise/page5.php
+++ SessionExercise/page5.php
@@ -1,15 +1,18 @@
 <?php
-//start a session
 @session_start();
-//get session's value
-$username = $_SESSION["username"];
-echo "Username:".$username;
-
-$password = $_SESSION["password"];
-echo "Password:".$password;
-
-$color = $_SESSION["color"];
-echo "Color:".$color;
-
-$address = $_SESSION["Address"];
-echo "Address:".$address;
+@session_destroy();
+?>
+<html>
+<head>
+    <title>Session Exercise</title>
+</head>
+<body>
+<ul>
+    <li><a href="page1.php">Page 1</a></li>
+    <li><a href="page2.php">Page 2</a></li>
+    <li><a href="page3.php">Page 3</a></li>
+    <li><a href="page4.php">Page 4</a></li>
+    <li><a href="page5.php">Page 5</a></li>
+</ul>
+</body>
+</html>
