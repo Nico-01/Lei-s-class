1c5265df2f4e203c96b6d78751d6358f96bf1a12
Nico-01 <nicolegaskell.contact@gmail.com>
2020-12-04 08:40:39 +1300

    fix?

--- staticWeb/WEBSITE/checkout.php
+++ staticWeb/WEBSITE/checkout.php
@@ -81,7 +81,7 @@ function createAnOrder($userID, $shippingAddress, $datetime){
     //1 connection
     $conn = createDatabaseConnection();
     //2 query
-    $sql = "(NULL,$userID,'$shippingAddress','$datetime')";
+    $sql = "($userID,'$shippingAddress','$datetime')";
     echo $sql;
     //3 run query
     $result = mysqli_query($conn, $sql);
